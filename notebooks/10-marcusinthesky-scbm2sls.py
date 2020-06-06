900541bb00191963b7ccc6e6b3e190d0095e4f8e
marcusinthesky <gwrmar002@myuct.ac.za>
2020-06-06 21:57:16 +0200

    g2sls

--- notebooks/10-marcusinthesky-scbm2sls.py
+++ notebooks/10-marcusinthesky-scbm2sls.py
@@ -12,6 +12,7 @@ import seaborn as sns
 import statsmodels.api as sm
 from pysal import explore
 from scipy import stats
+from scipy.optimize import minimize
 from scipy.stats import norm, poisson
 from statsmodels.regression.linear_model import OLS
 
@@ -162,50 +163,92 @@ results.summary()
 samples = renamed_distances.replace(0, np.nan).melt().value.dropna()
 
 # %%
-distribution = stats.pareto(*stats.pareto.fit(samples))  # 3)
-D = (
-    renamed_distances.loc[features.index, features.index]
-    .replace(0, np.nan)
-    .apply(distribution.pdf)
-    .fillna(0)
+b, loc, scale = stats.pareto.fit(samples)
+
+
+def hyperparam(x):
+    b, loc, scale = x
+    distribution = stats.pareto(b, loc, scale)
+    # k, mu = x
+    # distribution = stats.poisson(k, mu)
+    D = (
+        renamed_distances.loc[features.index, features.index]
+        .replace(0, np.nan)
+        .apply(distribution.pdf)
+        .fillna(0)
+    )
+
+    # G = D
+    G = D.apply(lambda x: x / np.sum(x), 1)
+
+    I = np.identity(G.shape[1])
+    X = features.drop(columns=["alpha", "returns"])
+    y = features.loc[:, ["returns"]]
+    IV = pd.concat([(I - G) @ X, (I - G) @ G @ X, (I - G) @ G @ G @ X], axis=1)
+    Xt = pd.concat(
+        [
+            ((I - G) @ G @ y).rename(columns=lambda x: "endogenous"),
+            (I - G) @ X,
+            ((I - G) @ G @ X).rename(columns=lambda x: x + "_exogenous"),
+        ],
+        axis=1,
+    )
+
+    B = Xt.assign(alpha=1).drop(
+        columns=[
+            "profit_margin_exogenous",
+            "market_capitalization_exogenous",
+            # "rm_exogenous", "price_to_research_exogenous",'price_to_research',
+            "profit_margin",
+            "price_to_earnings",
+            "market_capitalization",
+        ]
+    )
+    ivsls = IV2SLS(y, B, IV)
+    ivsls_results = ivsls.fit()
+
+
+best_param = minimize(
+    lambda x: hyperparam(x)[0],
+    x0=[1.2455455044025334, -0.001699277293891539, 2.001699277208737],
+    method="Nelder-Mead",
+    tol=1e-6,
 )
 
-G = D
-I = np.identity(G.shape[1])
-X = features.drop(columns=["alpha", "returns"])
-y = features.loc[:, ["returns"]]  # .pipe(lambda x: x - x.mean())
-IV = pd.concat([(I - G) @ X, (I - G) @ G @ X, (I - G) @ G @ G @ X], axis=1)
-Xt = pd.concat(
-    [
-        ((I - G) @ G @ y).rename(columns=lambda x: "exogenous"),
-        (I - G) @ X,
-        ((I - G) @ G @ X).rename(columns=lambda x: x + "_endogenous"),
-    ],
-    axis=1,
-)
+r_squared, results, G = hyperparam(best_param.x)
+
+# summary
+results.summary()
 
 
-B = Xt.assign(alpha=1).drop(
-    columns=[
-        "profit_margin_endogenous",
-        "market_capitalization_endogenous",
-        "rm_endogenous",
-        "profit_margin",
-        "price_to_earnings",
-        "market_capitalization",
-    ]
-)
-ivsls = IV2SLS(y, B, IV.assign(constant=1))
-ivsls_results = ivsls.fit()
-ivsls_results.summary()
+# %%
+# probablity plot
+osm_osr, slope_intercept_r = stats.probplot(results.resid, dist="norm")
 
+(
+    pd.DataFrame(dict(zip(("Quantile", "Ordered Values"), osm_osr))).hvplot.scatter(
+        y="Ordered Values", x="Quantile"
+    )
+    * pd.DataFrame(
+        {
+            "Quantile": [np.min(osm_osr[0]), np.max(osm_osr[0])],
+            "Ordered Values": [
+                slope_intercept_r[0] * np.min(osm_osr[0]) + slope_intercept_r[1],
+                slope_intercept_r[0] * np.max(osm_osr[0]) + slope_intercept_r[1],
+            ],
+        }
+    ).hvplot.line(y="Ordered Values", x="Quantile", color="black")
+).opts(title="Probability Plot")
 
+stats.ttest_1samp(results.resid, 0, axis=0)
+
+# heteroskedasticity plots
 (
     reduce(
         add,
         [
             v.to_frame()
-            .assign(Residuals=ivsls_results.resid)
+            .assign(Residuals=results.resid)
             .hvplot.scatter(x=k, y="Residuals")
             for k, v in B.drop(columns=["alpha"])
             .rename(columns=lambda s: " ".join(s.split("_")).title())
@@ -214,3 +257,74 @@ ivsls_results.summary()
     )
     * hv.HLine(0).opts(color="black")
 ).cols(1)
+
+
+# %%
+# Heteroskedasticity tests¶
+## Breush-Pagan test:
+import statsmodels.stats.api as sms
+
+name = ["Lagrange multiplier statistic", "p-value", "f-value", "f p-value"]
+test = sms.het_breuschpagan(results.resid, results.model.exog)
+list(zip(name, test))
+
+## Goldfeld-Quandt test:
+name = ["F statistic", "p-value"]
+test = sms.het_goldfeldquandt(results.resid, results.model.exog)
+list(zip(name, test))
+
+# Linearity:
+name = ["t value", "p value"]
+test = sms.linear_harvey_collier(results)
+list(zip(name, test))
+
+
+# SLX
+Xt = X.drop(columns=["market_capitalization", "price_to_research", "price_to_earnings"])
+model = OLS(
+    y, Xt.assign(alpha=1, profit_margin_exogenous=G @ X.profit_margin.to_frame())
+).fit()
+model.summary()
+
+# SAR
+Xt = X.drop(columns=["market_capitalization", "price_to_research", "price_to_earnings"])
+model = OLS(y, Xt.assign(alpha=1, endogenous=G @ y)).fit()
+model.summary()
+
+# G2SLS
+S = pd.concat([(I - G) @ X, (I - G) @ G @ X, (I - G) @ G @ G @ X], axis=1)
+P = S @ pd.DataFrame(np.linalg.pinv(S.T @ S), index=S.columns, columns=S.columns) @ S.T
+Xt = pd.concat([(I - G) @ G @ y, (I - G) @ X, (I - G) @ G @ X], axis=1)
+Th2sls = (
+    pd.DataFrame(np.linalg.pinv(Xt.T @ P @ Xt), columns=Xt.columns, index=Xt.columns)
+    @ Xt.T
+    @ P
+    @ y
+).rename(columns={"returns": "Theta_2sls"})
+Zh = pd.concat(
+    [
+        (I - G) @ G @ (Xt @ Th2sls),
+        (I - G) @ X,
+        ((I - G) @ G @ X).rename(columns=lambda s: s + "_exogenous"),
+    ],
+    axis=1,
+)
+Thlle = (
+    pd.DataFrame(np.linalg.pinv(Zh.T @ Xt), columns=Zh.columns, index=Zh.columns)
+    @ Zh.T
+    @ y
+).rename(columns={"returns": "Theta_lle"})
+d = y.subtract((Zh @ Thlle).to_numpy()).pow(2).sum()
+D = pd.DataFrame(np.identity(Zh.shape[0]), columns=Zh.index, index=Zh.index).multiply(
+    d.item()
+)
+Vhlle = (
+    pd.DataFrame(np.linalg.pinv(Zh.T @ Xt), index=Zh.columns, columns=Zh.columns)
+    @ Zh.T
+    @ D
+    @ Zh
+    @ pd.DataFrame(np.linalg.pinv(Zh.T @ Xt), index=Zh.columns, columns=Zh.columns)
+)
+SE_Vhlle = pd.DataFrame(np.diag(Vhlle), index=Vhlle.columns, columns=["SE"])
+T_lle = (Thlle / SE_Vhlle.to_numpy()).rename(columns={"Theta_lle": "T_lle"})
+p = T_lle.apply(stats.t(df=139).sf).rename(columns={"T_lle": "p_lle"})
